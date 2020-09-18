35321702f142e3270acad07a514f8fad642fb827
wlgud0402 <wlgudrlgus@naver.com>
2020-09-18 23:21:34 +0900

    재귀로 구현한 이진탐색

--- 탐색/이진탐색/재귀로구현.py
+++ 탐색/이진탐색/재귀로구현.py
@@ -8,8 +8,10 @@ def binary_search(array, target, start, end):
         return mid + 1  # 인덱스를 0번째부터가아니라 1번째로 도출하기위해 마지막 결과에 +1을 해준다.
     elif array[mid] > target:
         return binary_search(array, target, start, mid - 1)
+        # 타겟이보다 mid 의 값이 크다면 타겟은 mid보다 앞쪽에 있는것 =>  end를 mid -1 로 바꾼다.
     else:
         return binary_search(array, target, mid + 1, end)
+        # 타겟이 array[mid] 보다 크다면 타겟은 mid보다 뒤쪽의 범위에 있는것 => start를 mid +1 로 바꾼다.
 
 
 print(binary_search([1, 3, 4, 5, 6, 8, 9, 10], 6, 0, 7))
