39456d5f64fa37c63553a6c88b7b9d5fce7609a7
wlgud0402 <wlgudrlgus@naver.com>
2020-09-19 21:19:37 +0900

    부품찾기 with set()

---
+++ 탐색/이진탐색/부품찾기3.py
@@ -0,0 +1,13 @@
+# set을 사용하기
+
+n = int(input())
+array = set(map(int, input().split()))
+
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    if i in array:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
