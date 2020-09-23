72873f85e2269190eb0fd18a2a85fc88858c8581
wlgud0402 <wlgudrlgus@naver.com>
2020-09-23 18:16:30 +0900

    다익스트라

---
+++ 최단경로_다익스트라/간단한ver.py
@@ -0,0 +1,13 @@
+# import sys
+
+# input = sys.stdin.readline
+INF = int(1e9)
+
+
+graph = [[] for i in range(10)]
+visited = [False] * 10
+distance = [INF] * 10
+
+print(graph)
+print(visited)
+print(distance)
