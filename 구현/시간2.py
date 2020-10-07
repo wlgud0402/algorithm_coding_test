1f9528eef19875f9edd1a1bcbf0962f625a04da1
wlgud0402 <wlgudrlgus@naver.com>
2020-10-07 23:00:48 +0900

    second solve

---
+++ 구현/시간2.py
@@ -0,0 +1,11 @@
+def solution(n):
+    count = 0
+    for i in range(n+1):
+        for j in range(60):
+            for k in range(60):
+                if '3' in str(i) + str(j) + str(k):
+                    count += 1
+    return count
+
+
+print(solution(5))
