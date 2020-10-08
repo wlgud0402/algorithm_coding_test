1f5cdd180de371cc498c3eb31a246bf5d7b84d97
wlgud0402 <wlgudrlgus@naver.com>
2020-10-08 18:18:08 +0900

    failed

---
+++ 구현/게임개발.py
@@ -0,0 +1,7 @@
+def solution(position, looking, map_form):
+
+
+print(solution((1, 1), 0, [[1, 1, 1, 1],
+                           [1, 0, 0, 1],
+                           [1, 1, 0, 1],
+                           [1, 1, 1, 1]]))
